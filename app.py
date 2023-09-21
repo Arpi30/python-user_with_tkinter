99606c0c5ac902443cdc29f52bef2e87ead218a9
Ács Árpád <alijie24@gmail.com>
2023-09-21 21:53:21 +0200

    add edit and entered function,save the new data in db

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 from db_manager import *
-import random
 
-rand_int = random.randint(1, 1000)
+
 win = Tk()
-win.geometry("1020x640")
+win.geometry("1210x700")
 win.title("Users")
 win.resizable(False, False)
 
@@ -31,40 +30,50 @@ select_gender.grid(row=2, column=2, padx=5, pady=5, columnspan=2)
 Label(win, text="Score", font="Helvetica 12 bold").grid(
     row=3, column=0, columnspan=1)
 score = Entry(win, font="Helvetica 20 bold")
-score.grid(row=3, column=2, padx=5, pady=20, columnspan=2)
+score.grid(row=3, column=2, padx=5, columnspan=2)
+
+Label(win, text="Permission", font="Helvetica 12 bold").grid(
+    row=4, column=0, columnspan=1)
+permission = StringVar(win)
+permission.set("Permission")
+select_permission = OptionMenu(
+    win, permission, "A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3")
+select_permission.grid(row=4, column=2, padx=5, pady=20, columnspan=2)
 
 Label(win, text="Search as ID", font="Helvetica 12 bold").grid(
-    row=5, column=0, columnspan=1)
+    row=6, column=0, columnspan=1)
 search = Entry(win, font="Helvetica 20 bold")
-search.grid(row=5, column=1, columnspan=2)
+search.grid(row=6, column=1, columnspan=2)
 
 
-cols = ("id", "Name", "Age", "Gender", "Score")
+cols = ("id", "Name", "Age", "Gender", "Score", "Permission")
 table = ttk.Treeview(win, columns=cols, show="headings")
 for col in cols:
     table.heading(col, text=col, anchor='center')
     table.column(col, anchor="center")
-table.grid(row=6, column=0, columnspan=4, padx=5, pady=5)
+table.grid(row=7, column=0, columnspan=4, padx=5, pady=5)
+
+table.bind("<Double-1>", lambda e: on_double_click(table, e, win))
 
 insert_button = Button(win, text="Insert data",
-                       font="Helvetica 12 bold", command=lambda: create_user(rand_int, name, age, gender, score))
-insert_button.grid(row=4, column=0)
+                       font="Helvetica 12 bold", command=lambda: create_user(name, age, gender, score, permission))
+insert_button.grid(row=5, column=0)
 
 query_button = Button(win, text="Query data",
                       font="Helvetica 12 bold", command=lambda: db_query(table))
-query_button.grid(row=4, column=1)
+query_button.grid(row=5, column=1)
 
 export_csv = Button(win, text="Export to csv",
                     font="Helvetica 12 bold", command=export_to_csv)
-export_csv.grid(row=4, column=2)
+export_csv.grid(row=5, column=2)
 
 button_del = Button(win, text="Delete", font="Helvetica 12 bold",
                     command=lambda: delete(table))
-button_del.grid(row=4, column=3, pady=20)
+button_del.grid(row=5, column=3, pady=20)
 
 button_search = Button(
     win, text="Search", font="Helvetica 12 bold", command=lambda: search_user(search, table))
-button_search.grid(row=5, column=3, columnspan=2, pady=10)
+button_search.grid(row=6, column=3, columnspan=2, pady=10)
 
 win.mainloop()
 db_close()
--- db_manager.py
+++ db_manager.py
@@ -1,5 +1,6 @@
 import sqlite3
 from tkinter import *
+import random
 conn = None
 curs = None
 
@@ -9,16 +10,17 @@ def db_create():
     conn = sqlite3.connect("users.db")
     curs = conn.cursor()
     curs.execute(
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER, name Text, age INTEGER, gender TEXT, score REAL)")
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER, name Text, age INTEGER, gender TEXT, score REAL, permission TEXT)")
 
 
-def create_user(id, name, age, gender, score):
-    if not name.get() or not age.get() or gender.get() == "Select you gender" or not score.get():
+def create_user(name, age, gender, score, permission):
+    rand_int = random.randint(1, 1000)
+    if not name.get() or not age.get() or gender.get() == "Select you gender" or not score.get() or permission.get() == "Permission":
         print("All field is mandatory")
         return
 
-    curs.execute("INSERT INTO users VALUES (?,?,?,?,?)",
-                 (id, name.get(), age.get(), gender.get(), score.get()))
+    curs.execute("INSERT INTO users VALUES (?,?,?,?,?,?)",
+                 (rand_int, name.get(), age.get(), gender.get(), score.get(), permission.get()))
     conn.commit()
     name.delete(0, 'end')
     age.delete(0, 'end')
@@ -51,13 +53,13 @@ def delete(table):
 
 
 def db_query(table):
-    curs.execute("SELECT * FROM users")
+    curs.execute("SELECT * FROM users ORDER BY name ASC")
     datas = curs.fetchall()
     table.delete(*table.get_children())
 
     for data in datas:
         table.insert("", "end", values=(
-            data[0], data[1], data[2], data[3], data[4]))
+            data[0], data[1], data[2], data[3], data[4], data[5]))
 
 
 def export_to_csv():
@@ -68,7 +70,8 @@ def export_to_csv():
     # Exportalas CSV file-ba
     with open("user.csv", "w", encoding="utf-8") as data:
         # write metodussal beallitjuk a csv header-jet. ;-vel valassza el majd a join metodussal hozzaadjuk a header ertekeit egy ilstben
-        data.write(";".join(["name", "age", "gender", "score"]))
+        data.write(
+            ";".join(["id", "name", "age", "gender", "score", "Permission"]))
         # at loopolunk a fetchelt listan
         data.write("\n")
         for d in datas:
@@ -76,6 +79,58 @@ def export_to_csv():
             data.write(";".join(str(item) for item in d))
             # newline karakter
             data.write("\n")
+
+
+def on_double_click(table, event, win):
+    # Az identify_region metodus vissza adja a treeview-ban levo pont koordinatait ami egz string lesz, pl: heading, cell, tree, stb..
+    region_clicked = table.identify_region(event.x, event.y)
+    # ami nem cell tipusu mezo azt nem adja vissza
+    if region_clicked not in ("cell"):
+        return
+    # vissza adja a treeview oszlopat
+    column = table.identify_column(event.x)
+    # Ezt a reszt nem ertem annyira, utana kell neznem!
+    colum_index = int(column[1:]) - 1
+
+    # vissza adja egy adott sor id-jat
+    selected_id = table.focus()
+    # vissza adja az adott sor elemeit egy dictben
+    selected_values = table.item(selected_id)
+    # vissza adja egy adott sor erteket egy tomben
+    selected_text = selected_values.get('values')[colum_index]
+
+    column_box = table.bbox(selected_id, column)
+
+    entry_edit = Entry(win, width=column_box[2], text=selected_text)
+    # Column index es item id, duppla kattintasra benne marad a cell erteke
+    entry_edit.editing_column_index = colum_index
+    entry_edit.editing_item_iid = selected_id
+    entry_edit.insert(0, selected_text)
+    entry_edit.select_range(0, END)
+    entry_edit.focus()
+    # eventek
+    entry_edit.bind("<FocusOut>", lambda e: e.widget.destroy())
+    entry_edit.bind("<Return>", lambda e,
+                    table=table: on_entered_pressed(e, table))
+
+    entry_edit.place(x=column_box[0] + 5, y=column_box[1] + 372,
+                     w=column_box[2], h=column_box[3])
+
+
+def on_entered_pressed(e, table):
+    new_text = e.widget.get()
+    selected_id = e.widget.editing_item_iid
+    column_index = e.widget.editing_column_index
+    column_name = table.heading(column_index)["text"]
+
+    current_values = table.item(selected_id).get("values")
+    current_values[column_index] = new_text
+    table.item(selected_id, values=current_values)
+
+    curs.execute(
+        f'UPDATE users SET {column_name} = ? WHERE id = ?', (new_text, current_values[0],))
+    conn.commit()
+    e.widget.destroy()
 
 
 def db_close():
